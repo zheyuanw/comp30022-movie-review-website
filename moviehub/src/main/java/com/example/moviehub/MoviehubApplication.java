4909c31a17a0494154d819113fca159e152028b2
chunxiw <chunxi.wang@student.unimelb.edu.au>
2022-09-12 16:03:41 +1000

    add spring boot initial package
    add spring boot initial package

---
+++ moviehub/src/main/java/com/example/moviehub/MoviehubApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviehubApplication.class, args);
+	}
+
+}
