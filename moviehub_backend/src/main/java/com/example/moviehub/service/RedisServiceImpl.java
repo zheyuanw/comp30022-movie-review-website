a5817a1244d0eab6d37ae58af8ef78d7b6997cda
zhaokaig <gzk747168814@gmail.com>
2022-09-22 11:58:02 +0800

    UserStory:Register -- RedisService is ready to be used

---
+++ moviehub_backend/src/main/java/com/example/moviehub/service/RedisServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.moviehub.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisServiceImpl implements RedisService{
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Override
+    public void setString(String key, String value) {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set(key, value,60*3, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public String getString(String key) {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        return ops.get(key);
+    }
+
+    @Override
+    public Boolean existKey(String key) {
+        try{
+            return stringRedisTemplate.hasKey(key);
+        }catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void updateString(String key, String value) {
+        stringRedisTemplate.delete(key);
+        setString(key, value);
+    }
+}
