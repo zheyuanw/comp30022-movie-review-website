5e9eb168143f1b328f9702f8c01974d884041033
zhaokaig <gzk747168814@gmail.com>
2022-09-27 00:50:39 +0800

    UserStory:Register & Login -- Formalise response of UserController to JSON String with format of {message:, body:}

--- moviehub_backend/src/main/java/com/example/moviehub/controller/UserController.java
+++ moviehub_backend/src/main/java/com/example/moviehub/controller/UserController.java
@@ -6,7 +6,9 @@ import com.example.moviehub.collection.User;
 import com.example.moviehub.service.Impl.RegisterServiceImpl;
 import com.example.moviehub.service.Impl.UserServiceImpl;
 import com.example.moviehub.util.JWTtokenUtil;
+import com.example.moviehub.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
@@ -32,28 +34,32 @@ public class UserController {
     }
 
     @PostMapping(value = "/email")
-    public String verifyEmail(@RequestBody RegisterForm registerForm) {
+    public ResponseEntity<String> verifyEmail(@RequestBody RegisterForm registerForm) {
         System.out.println(registerForm.getEmail());
         try{
             registerService.sendEmailVerificationCode(registerForm.getEmail());
         } catch (Exception e){
-            return e.getMessage();
+            return ResponseEntity.badRequest().body(JsonUtil.toJsonString(e.getMessage()));
         }
 
-        return "Email Sent";
+        return ResponseEntity.ok().body(JsonUtil.toJsonString("Email Sent"));
     }
 
     @PostMapping(path = "/register")
-    public String register(@RequestBody RegisterForm registerForm){
+    public ResponseEntity<String> register(@RequestBody RegisterForm registerForm){
+
+
         if (registerService.register(registerForm)){
-            return "Register succeeded";
+
+            return ResponseEntity.ok().body(JsonUtil.toJsonString("Register succeeded"));
         }else{
-            return "Register Failed";
+
+            return ResponseEntity.badRequest().body(JsonUtil.toJsonString("Register Failed"));
         }
     }
 
     @PostMapping(value = "/login")
-    public String login(@RequestBody User user) {
+    public ResponseEntity login(@RequestBody User user) {
         System.out.println(user);
 
 
@@ -62,9 +68,9 @@ public class UserController {
 
             UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
 
-            return JWTtokenUtil.generateToken(user).toString();
+            return ResponseEntity.ok(JsonUtil.toJsonString("Login succeed", JWTtokenUtil.generateToken(user)));
         }else{
-            return "Login Failed";
+            return ResponseEntity.badRequest().body(JsonUtil.toJsonString("Login failed"));
         }
 
     }
