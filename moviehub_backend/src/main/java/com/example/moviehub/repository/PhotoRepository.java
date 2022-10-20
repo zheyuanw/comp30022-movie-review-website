a06dd515989c8d4d235bade9a9eb25eb26323cbb
chunxiw <chunxiw@student.unimelb.edu.au>
2022-10-20 22:26:35 +1100

    fix avatar system

--- moviehub_backend/src/main/java/com/example/moviehub/repository/PhotoRepository.java
+++ moviehub_backend/src/main/java/com/example/moviehub/repository/PhotoRepository.java
@@ -4,6 +4,12 @@ import com.example.moviehub.collection.Photo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhotoRepository extends MongoRepository<Photo, String> {
+
+    List<Photo> findByUserid(String userid);
+
+    void deleteByUserId(String userid);
 }
