8d8ebc332f234c6537a1768c4aeeee787fafde60
chunxiw <chunxiw@student.unimelb.edu.au>
2022-11-08 03:12:51 +1100

    add dependency

--- moviehub_backend/src/main/java/com/example/moviehub/MoviehubApplication.java
+++ moviehub_backend/src/main/java/com/example/moviehub/MoviehubApplication.java
@@ -1,8 +1,12 @@
 package com.example.moviehub;
 
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 
@@ -13,5 +17,9 @@ public class MoviehubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviehubApplication.class, args);
 	}
-
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer customizer()
+	{
+		return builder -> builder.serializerByType(ObjectId.class,new ToStringSerializer());
+	}
 }
